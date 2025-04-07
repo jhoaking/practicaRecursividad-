cb3a10f5dfe6f95346cafb616537d7c1654e63c7
jhoaking <peredoroj@gmail.com>
2025-04-06 20:38:52 -0400

    tarea de recursividad

---
+++ recursividad.java
@@ -0,0 +1,25 @@
+
+//este ejercicio trata sobre la manera de reescribir una potenci en base 2 
+public class Potencia
+{
+   public String mostrarNotacion(int k){
+      String res = "";
+             
+        if(k == 0){
+             res = "1";
+        }else if(k == 1){
+            res = "2";
+        }else {
+            if(k % 2  != 0){
+                res = "(2 *" + ")" +"^" + mostrarNotacion(k-1) ;
+            }else 
+                res  =  "(2" + ")" +"^" + mostrarNotacion(k/2) ;
+            
+        }
+        
+        return res;
+    }
+  
+    
+        
+}
