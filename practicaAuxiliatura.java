a7f380d4bb8f261999cd135e7fa35740414b4706
jhoaking <peredoroj@gmail.com>
2025-04-10 15:48:35 -0400

    practica recur umms

--- practicaAuxiliatura.java
+++ practicaAuxiliatura.java
@@ -13,7 +13,7 @@ public class Potencia
             if(k % 2  != 0){
                 res = "(2 *" + ")" +"^" + mostrarNotacion(k-1) ;
             }else 
-                res  =  "(2" + ")" +"^" + mostrarNotacion(k/2) ;
+                 res = "(" + res + ")"+ "^" + generarNotacion(k/2);
             
         }
         
@@ -22,4 +22,25 @@ public class Potencia
   
     
         
+}
+
+// este ejercicio trata de dar una plabra y mediante las posiciones pares va obteniendo los caracteres y va creando una nueva palabra
+public class Etimology {
+
+    public String nuevaPalabra(String s, int k) {
+        if(k <= 0){
+            return s;
+        }else{
+         return nuevaPalabra(s+s,k,0,"");
+        }
+        
+    }
+
+    private String nuevaPalabra(String s,  int k,int pos, String res) {
+        if(pos < s.length() -1){
+            return nuevaPalabra(s,k,pos+2, res + s.charAt(pos));
+        }else{
+            return nuevaPalabra(res, k - 1);
+        }
+    }
 }
