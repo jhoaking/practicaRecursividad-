14d4f29bf5326e142258fc2da2656c1eed99c1fd
jhoaking <peredoroj@gmail.com>
2025-04-14 18:43:57 -0400

    nuevos ejercicios

--- PracticaAuxiliatura/creadorPalabras.java
+++ PracticaAuxiliatura/creadorPalabras.java
@@ -1,29 +1,3 @@
-// a todos los ejercicios de aca los hago correr en BLUEJ
-
-public class Potencia
-{
-   public String mostrarNotacion(int k){
-      String res = "";
-             
-        if(k == 0){
-             res = "1";
-        }else if(k == 1){
-            res = "2";
-        }else {
-            if(k % 2  != 0){
-                res = "(2 *" + ")" +"^" + mostrarNotacion(k-1) ;
-            }else 
-                 res = "(" + res + ")"+ "^" + generarNotacion(k/2);
-            
-        }
-        
-        return res;
-    }
-  
-    
-        
-}
-
 // este ejercicio trata de dar una plabra y mediante las posiciones pares va obteniendo los caracteres y va creando una nueva palabra
 public class Etimology {
 
---
+++ PracticaAuxiliatura/sumaDigitos.java
@@ -0,0 +1,25 @@
+  
+public class Ejercicio{
+   public int cantidadDigito(int n){
+       return cantidadDigito(n, 0);
+    }
+    
+   private int cantidadDigito (int n , int suma){
+        int res  = 0;
+        if(n == 0){
+             if(suma < 10){
+                res = suma;
+            }else {
+                res = cantidadDigito(suma,0);
+                }
+        }else {
+            int digi = n %10;
+            res = cantidadDigito(n/10, suma + digi);
+        }
+        return res;
+    } 
+    
+}
+    
+     
+
